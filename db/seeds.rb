5c37220590f35ceda5b375249daf76a98a537555
MichelaCapece <michelacapece96@gmail.com>
2019-04-01 14:14:24 +0200

    risoluzione problema post più piaciuto

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: "Amministratore",
-             nickname: "admin",
-             email: "example@railstutorial.org",
-             password: "foobar",
-             password_confirmation: "foobar",
-             admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "Michela Capece",
-             nickname: "michela",
-             email: "michelacapece96@gmail.com",
-             password: "michela",
-             password_confirmation: "michela",
-             admin: false,
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name: "Flavia Ferranti",
-             nickname: "flavia",
-             email: "fla.ferranti97@gmail.com",
-             password: "flavia",
-             password_confirmation: "flavia",
-             admin: false,
-             activated: true,
-             activated_at: Time.zone.now)
-
-
 
